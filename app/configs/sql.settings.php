b5880288ea1f7f5abd8e716a103f1d98a18e3247
Ahmed Ibrahim <siddiqui.ahmedibrahim@gmail.com>
2023-08-07 17:32:54 +0500

    change config

--- app/configs/sql.settings.php
+++ app/configs/sql.settings.php
@@ -2,7 +2,7 @@
 if(!defined("CryptExchanger_INSTALLED")) { header("HTTP/1.0 404 Not Found"); exit; }
 $CONF = array();
 $CONF["host"] = "localhost";
-$CONF["user"] = "samdefic_sam";
-$CONF["pass"] = "N}l8uo~B]t0b";
-$CONF["name"] = "samdefic_sam";
+$CONF["user"] = "root";
+$CONF["pass"] = "";
+$CONF["name"] = "samdefi";
 ?>
